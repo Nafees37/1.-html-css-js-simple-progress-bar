92689c05748c2c1d409f5b098d75bd45f464acea
Khandaker Md. Saiful Islam <91514767+Nafees37@users.noreply.github.com>
2022-09-23 00:44:21 +0600

    Add files via upload

---
+++ html css js simple progress bar/custom.js
@@ -0,0 +1,15 @@
+function myfun() {
+  document.getElementById("div1").style.background = "#00a8ff";
+
+  document.getElementById("div2").style.background = "#009432";
+
+  document.getElementById("div3").style.background = "#006266";
+}
+
+function myfun1() {
+  document.getElementById("div1").style.background = "#006266";
+
+  document.getElementById("div2").style.background = "#1B1464";
+
+  document.getElementById("div3").style.background = "#00a8ff";
+}
